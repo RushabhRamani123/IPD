32a58b504f58075e3c77d3a320ba732f1e7ff185
rushabh ramani <rushabhramani16@gmail.com>
2024-11-05 08:05:08 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+/* eslint-disable no-unused-vars */
+import { Routes, Route } from 'react-router-dom';
+import TranslationUI from './Bot/Chatbot';
+import './index.css'
+function App() {
+  return (
+    // <Routes>
+    //   <Route path="/bot" element={<TranslationUI/>} />
+    // </Routes>
+    <> 
+    <TranslationUI />    
+    </>
+  );
+}
+export default App;
